fcbd196f288de1195df9d2dc79232b0dd7929388
Пользователь <pumalcn03@gmail.com>
2020-04-19 01:56:42 +0600

    week3 is deleted

---
+++ midterm/midterm/main/constants.py
@@ -0,0 +1,13 @@
+Journal_Bullet = 1
+Journal_Food = 2
+Journal_Sport = 3
+Journal_Travel = 4
+
+JOURNAL_TYPES =(
+    (Journal_Bullet, 'Bullet'),
+    (Journal_Food, 'Food'),
+    (Journal_Sport,'Sport'),
+    (Journal_Travel, 'Travel'),
+)
+
+
